2f46b9aeff7b245e424daaf781a0dbd1023ae903
priyam-v <v.priyamvadhaa.s92@kalvium.community>
2024-11-29 14:12:05 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 // Task 4 :-Create a for loop to print all the even numbers from 1 - 200 and return the same inside the function.You will need to create an empty array, create the logic and push the output to the array and return it inside the function.
 
-function loop(){
+function loop(){ const="even numbers"(1-100):
+                console.log(const)
 
 }
 // Task 5 :- create a variable of string datatype and return the string output inside the function given below
 
-function printStr(){
+function printStr(){var="boolean values"
+                    const="true or false"
+                    if "true"
+                    console.log(if)
+                     else:
+                    console.log(false)
   
 }
 
